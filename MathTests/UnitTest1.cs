0e618e1bbd6dd7d4183166cf3a8b1a39064f29b8
Lazer Porter <lazar.porter@gmail.com>
2019-08-07 22:00:26 -0400

    new test, broader range. fails

--- MathTests/UnitTest1.cs
+++ MathTests/UnitTest1.cs
@@ -20,5 +20,25 @@ namespace MathTests
             // Check the result:
             Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
         }
+
+        [TestMethod]
+        public void RooterValueRange()
+        {
+            // Create an instance to test.
+            Rooter rooter = new Rooter();
+
+            // Try a range of values.
+            for (double expected = 1e-8; expected < 1e+8; expected *= 3.2)
+            {
+                RooterOneValue(rooter, expected);
+            }
+        }
+
+        private void RooterOneValue(Rooter rooter, double expectedResult)
+        {
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000);
+        }
     }
 }
