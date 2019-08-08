3d2fac76610e633101e295173a66f62b74cdfcc9
Lazer Porter <lazar.porter@gmail.com>
2019-08-07 21:44:35 -0400

    test method added

--- MathTests/UnitTest1.cs
+++ MathTests/UnitTest1.cs
@@ -6,8 +6,18 @@ namespace MathTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BasicRooterTest()
         {
+            // Create a test instance:
+            Rooter rooter = new Rooter();
+            // Define a test input and expected result:
+            double input = expectedResult * expectedResult;
+            double expectedResult = 2.0;
+            
+            // Call the method being tested:
+            double actualResult = rooter.SquareRoot(input);
+            // Check the result:
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
         }
     }
 }
